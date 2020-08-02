4bce763d62b47c3ba6a294cbc7e20230c46cf54a
Jason Kollias <jason.kollias@kissusa.com>
2020-08-02 12:47:41 -0400

    added js and css dynamically

--- assets/js/delta-difference.js
+++ assets/js/delta-difference.js
@@ -1,4 +1,5 @@
 /* Errors */
+/* TODO: Rewrite these using try catch */
 
 function checkIfImageExists(file){
     if(!file){
@@ -19,15 +20,12 @@ function checkImageExtensionIsValid(file){
             validExtension = true;
             return true;
         }else{
-            console.log("Extension is not valid.");
+            console.log("Extension is not valid. Please use jpg, gif, png, svg, tiff, webp, or ico");
             validExtension = false;
-
         }
-
     }
     return validExtension;
 }
-
 
 
 function checkWidth(width){
@@ -38,52 +36,7 @@ function checkWidth(width){
     }
 }
 
-
-/* Create Controls */
-function deltaDifferenceTitle(){
-    let title = document.createElement("h1");
-        title.classList.add("delta-difference-heading");
-        title.setAttribute("onClick","toggle_controls();");
-    let titelText = document.createTextNode("The Delta Difference");
-        title.appendChild(titelText);
-    return title;
-}
-
-function deltaDifferenceToggleButton(){
-    let button = document.createElement("button");
-        button.classList.add("toggle-delta-button");
-        button.setAttribute("onClick","toggle_mockup();");
-    let buttonText = document.createTextNode("Toggle");
-        button.appendChild(buttonText);
-    return button;
-}
-
-function deltaDifferenceMoveUpButton(){
-    let controls = document.createElement("div");
-        controls.classList.add("delta-difference__move")
-        controls.innerHTML =   "<input type='text' id='up-ammount' placeholder='Enter Number' value=''>";
-        controls.innerHTML +=  "<button class='move-delta-button' onClick='move_mockup();'>Move MockUp</button>";
-    return controls;
-}
-
-function deltaDifferenceOpacityButton(){
-    let button = document.createElement("button");
-        button.classList.add("opacity-delta-button");
-        button.setAttribute("onClick","change_delta_opacity();");
-    let buttonText = document.createTextNode("Change Opacity");
-        button.appendChild(buttonText);
-    return button;
-}
-
-function deltaDifferenceControls(){
-    let div = document.createElement("div");
-        div.classList.add("delta-difference__controls");
-        div.appendChild(deltaDifferenceTitle());
-        div.appendChild(deltaDifferenceToggleButton());
-        div.appendChild(deltaDifferenceMoveUpButton());
-        div.appendChild(deltaDifferenceOpacityButton());
-    return div;
-}
+/* Errors End */
 
 
 /* Image */
@@ -97,22 +50,44 @@ function deltaDifferenceMockUpImage(file, width){
     return mockup;
 }
 
+/* Resources */
+// <link href="delta-difference.css" rel="stylesheet" type="text/css">
+
+
+function addCssFile(file_name){
+     var linkTag = document.createElement('link');
+     linkTag.setAttribute('href',"assets/css/"+file_name);
+     linkTag.setAttribute('rel',"stylesheet");
+     linkTag.setAttribute('type',"text/css");
+     document.head.appendChild(linkTag);
+     console.log("assets/css/"+file_name);
+}
+
+
+function addJavaScriptFile(script_file_name){
+     var scriptTag = document.createElement('script');
+     scriptTag.setAttribute('src',"assets/js/"+script_file_name);
+     document.body.appendChild(scriptTag);
+}
 
 /* Main function */
 
-
 function deltaDifference(file,file_width){
+    addCssFile("delta-difference.css");
+
     checkIfImageExists(file);
     checkWidth(file_width);
     let deltaDifferenceDiv = document.createElement("div");
         deltaDifferenceDiv.id = "delta-difference";
-        deltaDifferenceDiv.appendChild(deltaDifferenceControls());
         deltaDifferenceDiv.appendChild(deltaDifferenceMockUpImage(file,file_width));
     document.body.appendChild(deltaDifferenceDiv);
+
+    addJavaScriptFile("create-control-elements.js");
+    addJavaScriptFile("control-functions.js");
 }
 
-
 /* Main controller functions */
+
 
 function toggle_mockup(){
     mockUp = document.getElementById("delta-difference__mock-up");
