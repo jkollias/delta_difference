2452ed278a6b11942272699b6e2281e7fd9a6ff0
Jason Kollias <jason.kollias@kissusa.com>
2020-08-02 12:49:38 -0400

    added js and css dynamically

--- assets/js/delta-difference.js
+++ assets/js/delta-difference.js
@@ -51,8 +51,6 @@ function deltaDifferenceMockUpImage(file, width){
 }
 
 /* Resources */
-// <link href="delta-difference.css" rel="stylesheet" type="text/css">
-
 
 function addCssFile(file_name){
      var linkTag = document.createElement('link');
@@ -84,34 +82,4 @@ function deltaDifference(file,file_width){
 
     addJavaScriptFile("create-control-elements.js");
     addJavaScriptFile("control-functions.js");
-}
-
-/* Main controller functions */
-
-
-function toggle_mockup(){
-    mockUp = document.getElementById("delta-difference__mock-up");
-    mockUp.classList.toggle("hide-mockup");
-}
-
-function move_mockup(){
-    mockUp = document.getElementById("delta-difference__mock-up");
-    let topInc = document.getElementById("up-ammount").value;
-    mockUp.style.top = topInc+"px";
-    console.log("Mockup is moved "+ topInc);
-}
-
-function toggle_controls(){
-    main = document.getElementById("delta-difference");
-    main.classList.toggle("hide-controls");
-}
-
-function change_delta_opacity(){
-    let current_opacity = document.getElementById("delta-difference__mock-up").style.opacity;
-    current_opacity = Number(current_opacity);
-    if(current_opacity <= .75 ){
-        document.getElementById("delta-difference__mock-up").style.opacity = current_opacity + .25;
-    }else{
-        document.getElementById("delta-difference__mock-up").style.opacity = 0;
-    }
 }
